e7705b7b633a67c08fa5908b8efa657aef98f991
laraalvesfreitas <laraalves624@gmail.com>
2026-02-25 20:28:24 -0300

    Conteúdo aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+//Exemplo While 
+
+// var c = 1
+
+// while(c <= 6){
+//     console.log('Ok?')
+//     c++
+// }
+
+
+//Exemplo do while
+
+var c = 1
+
+do{
+    console.log('Ok?')
+    c++
+}while(c <= 6)
+
+// console.log('OK??')
+
